10b9a9c517712b0a11a4f5127407b5943789881c
Jesse Ma <j.masonoma17@gmail.com>
2019-07-26 01:29:11 -0700

    Upload first exercise program

---
+++ 01-NodeConsole/helloworld.js
@@ -0,0 +1,6 @@
+console.log('Hello to Jesse\'s first NodeJS script');
+
+var curr_date = new Date().getFullYear();
+var birth_date = 1991;
+
+console.log(`I am ${curr_date - birth_date} years old as of today`);
