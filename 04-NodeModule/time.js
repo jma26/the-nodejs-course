b3e5b0577b82ee3a33c5d77a895cbeed1514a29e
Jesse Ma <j.masonoma17@gmail.com>
2019-07-30 18:48:38 -0700

    Add solution to 04-NodeModule: Days Til Module

--- 04-NodeModule/time.js
+++ 04-NodeModule/time.js
@@ -1,5 +1,3 @@
-var currentDate = new Date();
-
 var Time = function() {};
 
 Time.prototype = {
@@ -9,7 +7,16 @@ Time.prototype = {
   displayMilitaryTime: function() {
     console.log('Right now the time is ' + this.hours + ':' + this.minutes + ':' + this.seconds + ' military time');
   },
-  
+  displayDaysUntilChristmas: function() {
+    // Christmas Date
+    var christmas = new Date(new Date().getFullYear(), 11, 25);
+    if (new Date().getMonth() === 11 && new Date().getDate() > 25) {
+      // Set the full year of Christmas if it's past Decembember 25th
+      christmas.setFullYear(christmas.getFullYear() + 1);
+    }
+    var oneDay = 24 * 60 * 60 * 1000;
+    console.log(Math.ceil((christmas.getTime() - new Date().getTime()) / (oneDay))+ ' days until Christmas');
+  }
 }
 
 module.exports = Time;
