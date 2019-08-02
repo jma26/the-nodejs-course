2ed9816b31203a35c342fa4973c0b4b94e72b07a
Jesse Ma <j.masonoma17@gmail.com>
2019-08-02 13:27:19 -0700

    06-Gulp: Setup default behavior of gulp cmd

--- 06-Gulp/gulpfile.js
+++ 06-Gulp/gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
+// Gulp Task to manually jsLint
 gulp.task('jsLint', function() {
-  gulp.src(__dirname + '/js/demo.js')
+  return gulp.src('js/*.js')
   .pipe(jshint())
   .pipe(jshint.reporter());
 });
+
+// Setup default behavior of gulp when 'gulp' cmd is executed
+gulp.task('default', gulp.series('jsLint', function() {
+  return gulp.watch('js/*.js', gulp.series('jsLint'));
+}));
