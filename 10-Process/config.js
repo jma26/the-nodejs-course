da872654299528ce22533f1b72efe0aa24dac5ce
Jesse Ma <j.masonoma17@gmail.com>
2019-08-09 16:13:56 -0700

    Add solution to 10-Process: Configure your node app

---
+++ 10-Process/config.js
@@ -0,0 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    port: 3000,
+    prettyHtml: true
+  },
+  production: {
+    port: process.env.PORT || 5000,
+    prettyHtml: false
+  }
+};
+
+module.exports = config[env];
--- 10-Process/server.js
+++ 10-Process/server.js
@@ -1,17 +1,30 @@
-env = process.env.NODE_ENV || 'development';
-var config = {
-  development: {
-    port: 3000,
-    db: 'mongodb://localhost/mydb',
-    prettyHtml: true
-  },
-  production: {
-    port: process.env.PORT || 5000,
-    db: 'mongodb://mongolab.com/mydb',
-    prettyHtml: false
+// CommonJS Module
+var http = require('http');
+var fs = require('fs');
+var config = require('./config');
+var port = config.port;
+
+console.log(config);
+
+http.createServer(callback).listen(port);
+
+function callback(request, response) {
+  if (request.url === '/about') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile(__dirname + '/views/about.html', function(err, data) {
+      response.end(data);
+    });
+  } else if (request.url === '/jessema') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile(__dirname + '/views/awesome.html', function(err, data) {
+      response.end(data);
+    });
+  } else {
+    response.writeHead(400, {'Content-Type': 'text/html'});
+    fs.readFile(__dirname + '/views/404.html', function(err, data) {
+      response.end(data);
+    });
   }
-};
+}
 
-console.log(process.argv);
-
-module.exports = config[env];
+console.log('Server running at http://localhost:' + port);
