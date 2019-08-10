ed7a6239cc30e65c186db927ee93398043cc38ef
Jesse Ma <j.masonoma17@gmail.com>
2019-08-10 16:46:19 -0700

    Add solution to 11-Middleware: Static files

--- 11-MiddleWare/server.js
+++ 11-MiddleWare/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var http = require('http');
+var jade = require('jade');
 var port = 4567;
 
 var app = express();
 
 http.createServer(app).listen(port);
+
+// Serving static content
+app.use('/public', express.static(__dirname + '/public'))
 
 // Custom Middleware to use with next()
 app.use(function(request, response, next) {
@@ -13,8 +17,18 @@ app.use(function(request, response, next) {
 });
 
 app.get('/', function(request, response) {
-  response.writeHead(200);
-  response.write('Hello from 11-MiddleWare with express');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  // fs.readFile() is not necessary. Jade API has compileFile() that compiles directly from a file
+  var data = {
+    name: 'Jesse Ma'
+  };
+
+  var template = jade.compileFile(__dirname + '/public/index.jade');
+
+  var html = template(data);
+
+  // Output jade template to response object
+  response.write(html);
   response.end();
 });
 
